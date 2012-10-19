a8ed2bcfcd70345a18da1fb401486fc21cbb09de
Nah bro <15maddog@gmail.com>
2012-10-18 21:05:05 -0500

    Updated the menu class, added a new layout, q

--- src/org/vandymobile/dining/MenuListItem.java
+++ src/org/vandymobile/dining/MenuListItem.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class MenuListItem {
     private final int count;
     private final List<String> menus;
+    private final String mTitle;
     
-    MenuListItem(List<String> x){
+    MenuListItem(List<String> x, String title){
         menus = x;
         count = x.size();
+        mTitle = title;
     }
     
     public int getCount(){
@@ -18,5 +20,8 @@ public class MenuListItem {
     public List<String> getMenus(){
         return menus;
     }
-
+    
+    public String getTitle(){
+    	return mTitle;
+    }
 }
